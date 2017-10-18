37110fb3a45335cd466c6db7e26bc956dfbdba6f
段晗昭 <1689416292@qq.com>
2017-10-18 17:20:16 +0800

    first commit

---
+++ wawa/src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+
+
+Vue.use(Router)
+
+
+export default new Router({
+  routes: [
+    // {
+    //   // path: '/paihang',  // 路径
+    //   // component: Home // 对应的组件
+    // }
+  ]
+})
